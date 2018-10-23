e1151207885da1a91dcace9dcb36729268d76889
Ucnt <matt.svensson@gmail.com>
2018-10-23 16:41:39 -0400

    Update search_webpages.py

--- search_webpages.py
+++ search_webpages.py
@@ -10,9 +10,10 @@ from lib.constants import *
 from lib.arg_parser import *
 from lib.logger import *
 from lib.get_subpages import get_domain
+from lib.progressbar import *
 from module.find_s3_writable_buckets import find_writable_buckets
+import multiprocessing
 import json
-
 
 
 def get_searched_sites():
@@ -35,13 +36,43 @@ def get_urls(args, rerun):
     if args.webpage:
         urls.append(args.webpage)
     if args.webpage_list:
+        f = open(args.webpage_list, "r")
+        lines = list(set(f.readlines()))
         if not rerun:
-            logger.log.critical("Removing %s previously run sites.  Give me a sec..." %(len(searched_sites)))
-        for line in open(args.webpage_list):
-            line = line.strip()
-            if line:
-                if get_domain(line) not in searched_sites or rerun:
-                    urls.append(line)
+            logger.log.critical("Getting domains from %s urls.  Give me a sec..." %(len(lines)))
+        progress = ProgressBar(num_items=len(lines))
+
+
+        pool_size = multiprocessing.cpu_count() * 2
+        pool = multiprocessing.Pool(processes=pool_size)
+        active_processes = []            #Store the processes until they are done
+        domains = []
+
+        for line in lines:
+            active_processes.append(pool.apply_async(get_domain, (line.strip(),)))
+
+        #Compile the domains as they come in. 
+        num_items = len(lines)
+        while active_processes:
+            for active_process in active_processes:
+                if active_process.ready():
+                    if active_process._value:
+                        domains.append(active_process._value)
+                    active_processes.remove(active_process)
+                    #Don't over-update the progressbar or it'll slow everything down.  Printing is SLOW
+                    if len(active_processes) % 100 == 0:
+                        progress(num_completed=len(lines) - len(active_processes) - progress.cur_item)
+            progress(num_completed=0)        
+            time.sleep(1)
+
+        #Everything's done!    
+        progress.done()
+        pool.close()
+        pool.join()
+
+        logger.log.critical("Removing %s previously run sites from the %s potential results.  Give me a sec..." %(len(searched_sites), len(domains)))
+        return list(set(domains) - set(searched_sites))
+
     return urls
 
 
